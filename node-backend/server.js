8f0d1ce819fbb7b5c8987e46f2849641eb77b8aa
Akilan <akil.dove@gmail.com>
2018-07-20 05:32:43 +0530

    mongo client connect error solved

--- node-backend/server.js
+++ node-backend/server.js
@@ -18,7 +18,7 @@ require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
 
-mongoose.connect(process.env.DB_URL, (err) => {
+mongoose.connect(process.env.DB_URL,{ useNewUrlParser: true },(err) => {
     if (err) {
         console.log(err);
     } else {
